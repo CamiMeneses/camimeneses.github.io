6ba01b2b44fd592a888423464430af7ea80c000b
CamiMeneses <CamiMeneses@github.com>
2020-09-29 02:28:46 -0500

    add skills logos

--- src/pages/skills/skills.component.jsx
+++ src/pages/skills/skills.component.jsx
@@ -1,77 +1,119 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
 import './skills.style.scss'
-import MyCarousal from '../../components/my-carousal/mycarousal.component';
+
+// Layout
+import { Container } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Fade from 'react-reveal/Fade'
 import Image from 'react-bootstrap/Image'
 
+import Fade from 'react-reveal/Fade'
 
+// Images
 import Profile from '../../assets/img/profile/profile.jpg'
 
+import Ruby from '../../assets/icons/skills/ruby-icon.png'
+import Rails from '../../assets/icons/skills/rails.png'
+
+import Html from '../../assets/icons/skills/html.png'
+import Css from '../../assets/icons/skills/css.png'
+
+import Git from '../../assets/icons/skills/git.webp'
+import Github from '../../assets/icons/skills/github.png'
+import Bitbucket from '../../assets/icons/skills/bitbucket.png'
+
+import Scrum from '../../assets/icons/skills/scrum.png'
+import Jira from '../../assets/icons/skills/jira.png'
+import Trello from '../../assets/icons/skills/trello.png'
 
 export default function Contact(props) {
   return (
     <div>
       <div id="skills" className="section">
-      <h1>SKILLS</h1>
+        <h1>SKILLS</h1>
         <div id="skills-box">
           <Container>
             <Row className='pt-3 pb-5 align-items-center'>
-            <Col xs={12} sm={12} md={12} lg={3}>
-              <Fade bottom>
-                <Row className='justify-content-center mb-2 mr-2 education-box'>
-                  <Image className='justify-content-end' alt='profile' src={Profile} rounded fluid />
-                </Row>
-              </Fade>
-            </Col>
+              <Col xs={12} sm={12} md={12} lg={6}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Ruby} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={6}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Rails} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+            </Row>
 
-            <Col xs={12} sm={12} md={12} lg={6}>
-              <Fade bottom>
-                <Row className='justify-content-center mb-2 mr-2 education-box'>
-                  <Image className='justify-content-end' alt='profile' src={Profile} rounded fluid />
-                </Row>
-              </Fade>
-            </Col>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={4}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Git} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={4}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Github} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={4}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Scrum} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+            </Row>
 
-          <Col xs={12} sm={12} md={12} lg={3}>
-              <Fade bottom>
-                <Row className='justify-content-center mb-2 mr-2 education-box'>
-                  <Image className='justify-content-end' alt='profile' src={Profile} rounded fluid />
-                </Row>
-              </Fade>
-            </Col>
-          </Row>
+            <Row className='pt-3 pb-5 align-items-center'>
+              <Col xs={12} sm={12} md={12} lg={6}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Html} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={6}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Css} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+            </Row>
 
-          <Row className='pt-3 pb-5 align-items-center'>
-            <Col xs={12} sm={12} md={12} lg={3}>
-              <Fade bottom>
-                <Row className='justify-content-center mb-2 mr-2 education-box'>
-                  <Image className='justify-content-end' alt='profile' src={Profile} rounded fluid />
-                </Row>
-              </Fade>
-            </Col>
-
-            <Col xs={12} sm={12} md={12} lg={6}>
-              <Fade bottom>
-                <Row className='justify-content-center mb-2 mr-2 education-box'>
-                  <Image className='justify-content-end' alt='profile' src={Profile} rounded fluid />
-                </Row>
-              </Fade>
-            </Col>
-
-          <Col xs={12} sm={12} md={12} lg={3}>
-              <Fade bottom>
-                <Row className='justify-content-center mb-2 mr-2 education-box'>
-                  <Image className='justify-content-end' alt='profile' src={Profile} rounded fluid />
-                </Row>
-              </Fade>
-            </Col>
-          </Row>
-
-            {/*<MyCarousal />*/}
-
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={4}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Bitbucket} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={4}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Jira} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={4}>
+                <Fade bottom>
+                  <Row className='justify-content-center mb-2 mr-2 education-box'>
+                    <Image className='justify-content-end' alt='profile' src={Trello} rounded fluid />
+                  </Row>
+                </Fade>
+              </Col>
+            </Row>
           </Container>
         </div>
       </div>
