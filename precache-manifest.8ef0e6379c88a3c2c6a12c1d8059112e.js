3f6f3efc0ea322908fcb3b524b729b46589af877
CamiMeneses <CamiMeneses@github.com>
2020-12-30 21:33:35 -0500

    Updates

---
+++ precache-manifest.8ef0e6379c88a3c2c6a12c1d8059112e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971c00584be5d26b55db792db9a6499d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a827a0fe909253c99416",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "25d0ce08f43174300892",
+    "url": "/static/css/main.371b7dfd.chunk.css"
+  },
+  {
+    "revision": "a827a0fe909253c99416",
+    "url": "/static/js/2.d6c32aab.chunk.js"
+  },
+  {
+    "revision": "a5f655d830d088aa9894c35a94634b59",
+    "url": "/static/js/2.d6c32aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d0ce08f43174300892",
+    "url": "/static/js/main.e09fcc87.chunk.js"
+  },
+  {
+    "revision": "ef5d77dd4d7885cc0476",
+    "url": "/static/js/runtime-main.36a00852.js"
+  },
+  {
+    "revision": "b9ab61f2da0610cdcf307b8d9c400f4c",
+    "url": "/static/media/about.b9ab61f2.png"
+  },
+  {
+    "revision": "a7a81780e390fafe39fa3a90ca98c00a",
+    "url": "/static/media/bb.a7a81780.png"
+  },
+  {
+    "revision": "802f372b7e79a8a826a820a5ea11276c",
+    "url": "/static/media/bitbucket.802f372b.png"
+  },
+  {
+    "revision": "1cc55961780cf8b94ea4d4bd6473b182",
+    "url": "/static/media/css.1cc55961.png"
+  },
+  {
+    "revision": "b7cb36699ff4c8b0fb582665963fe981",
+    "url": "/static/media/distrital.b7cb3669.png"
+  },
+  {
+    "revision": "32f32ba1f05000eb00fbf767666b1e52",
+    "url": "/static/media/ec.32f32ba1.png"
+  },
+  {
+    "revision": "80dacce2ef1f88f464a783c14300c3c7",
+    "url": "/static/media/education-croquis.80dacce2.png"
+  },
+  {
+    "revision": "585a830ac8efdbbbeaf74d17125e5a57",
+    "url": "/static/media/ef_credential.585a830a.png"
+  },
+  {
+    "revision": "c483e4e2d3e9d262038aa29f02462d02",
+    "url": "/static/media/eng.c483e4e2.webp"
+  },
+  {
+    "revision": "5f8bdc0417494a8e394ed96b530b4b0a",
+    "url": "/static/media/esp.5f8bdc04.png"
+  },
+  {
+    "revision": "b8997b02f1934ee1332ff06cb6ae1312",
+    "url": "/static/media/experience-fucsia-spots.b8997b02.jpg"
+  },
+  {
+    "revision": "a4fb8bca565abfea64ddb187e168149c",
+    "url": "/static/media/git.a4fb8bca.webp"
+  },
+  {
+    "revision": "8c603510138da0cc55aa61091cca7cd6",
+    "url": "/static/media/github.8c603510.png"
+  },
+  {
+    "revision": "f6afbbbc1c7d9ecf3a0b77be5d1578f9",
+    "url": "/static/media/html.f6afbbbc.png"
+  },
+  {
+    "revision": "630cd59d584e3391598780d04e057b63",
+    "url": "/static/media/jira.630cd59d.png"
+  },
+  {
+    "revision": "9e620271dd4ba14ac240ec13d09db6ae",
+    "url": "/static/media/miriadax.9e620271.png"
+  },
+  {
+    "revision": "b2d3f7df72b7c9469af33fbf31a8dda1",
+    "url": "/static/media/profile.b2d3f7df.png"
+  },
+  {
+    "revision": "d899fa6d70324837c81efc9e8ad301a5",
+    "url": "/static/media/profile.d899fa6d.jpg"
+  },
+  {
+    "revision": "4dd71429939a43e7d6322ad6f5d2d279",
+    "url": "/static/media/rails.4dd71429.png"
+  },
+  {
+    "revision": "2d3730cf719b4f91586179f62a344ee7",
+    "url": "/static/media/ruby.2d3730cf.png"
+  },
+  {
+    "revision": "885b8e6e839390294bf0f136d4ffeb45",
+    "url": "/static/media/scrum.885b8e6e.png"
+  },
+  {
+    "revision": "58fe9fa7338262f97e5393a3510e6a0d",
+    "url": "/static/media/skills-background.58fe9fa7.png"
+  },
+  {
+    "revision": "0f459e5f38f179f945267a9b1f7b1e18",
+    "url": "/static/media/skills-girl-power.0f459e5f.jpg"
+  },
+  {
+    "revision": "5dba6f591a8ca8d6c90546d2b6641066",
+    "url": "/static/media/trello.5dba6f59.png"
+  },
+  {
+    "revision": "f5e27ddc094f5af1e5f43a3916b7ae18",
+    "url": "/static/media/unidad.f5e27ddc.png"
+  },
+  {
+    "revision": "96bec0eb560dcac44dd16b95521e948c",
+    "url": "/static/media/youse.96bec0eb.png"
+  }
+]);
