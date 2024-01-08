ccd88fbae1b7ccaa5aaed231eb88803a14976348
CamiMeneses <CamiMeneses@github.com>
2024-01-07 23:22:30 -0500

    modify skills style

--- src/pages/skills/skills.component.jsx
+++ src/pages/skills/skills.component.jsx
@@ -1,35 +1,34 @@
-import React from 'react'
-import './skills.style.scss'
+import React from "react";
+import "./skills.style.scss";
 
 // Layout
-import { Container } from 'react-bootstrap'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Image from 'react-bootstrap/Image'
-
-import Fade from 'react-reveal/Fade'
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Fade from "react-reveal/Fade";
 
 // Images
-import Ruby from '../../assets/icons/skills/ruby.png'
-import Rails from '../../assets/icons/skills/rails.png'
-import Nest from '../../assets/icons/skills/nest.png'
-import Html from '../../assets/icons/skills/html.png'
-import Css from '../../assets/icons/skills/css.png'
-import Typescript from '../../assets/icons/skills/typescript.png'
-import Rspec from '../../assets/icons/skills/rspec.png'
-import Jest from '../../assets/icons/skills/jest.png'
+import Ruby from "../../assets/icons/skills/ruby.png";
+import Rails from "../../assets/icons/skills/rails.png";
+import Nest from "../../assets/icons/skills/nest.png";
+import Html from "../../assets/icons/skills/html.png";
+import Css from "../../assets/icons/skills/css.png";
+import Typescript from "../../assets/icons/skills/typescript.png";
+import Rspec from "../../assets/icons/skills/rspec.png";
+import Jest from "../../assets/icons/skills/jest.png";
 
-import Postgres from '../../assets/icons/skills/postgres.png'
-import Mongo from '../../assets/icons/skills/mongo.png'
+import Postgres from "../../assets/icons/skills/postgres.png";
+import Mongo from "../../assets/icons/skills/mongo.png";
 
-import Git from '../../assets/icons/skills/git.webp'
-import Github from '../../assets/icons/skills/github.png'
-import Bitbucket from '../../assets/icons/skills/bitbucket.png'
-import Awss3 from '../../assets/icons/skills/awss3.png'
+import Git from "../../assets/icons/skills/git.webp";
+import Github from "../../assets/icons/skills/github.png";
+import Bitbucket from "../../assets/icons/skills/bitbucket.png";
+import Awss3 from "../../assets/icons/skills/awss3.png";
 
-import Scrum from '../../assets/icons/skills/scrum.png'
-import Jira from '../../assets/icons/skills/jira.png'
-import Trello from '../../assets/icons/skills/trello.png'
+import Scrum from "../../assets/icons/skills/scrum.png";
+import Jira from "../../assets/icons/skills/jira.png";
+import Trello from "../../assets/icons/skills/trello.png";
 
 export default function Skills(props) {
   return (
@@ -37,7 +36,7 @@ export default function Skills(props) {
       <div id="skills" className="section">
         <h1>SKILLS</h1>
         <div id="skills-box">
-          <Container>
+          <Container fluid="md">
             <Row>
               <Theme1 />
             </Row>
@@ -51,39 +50,61 @@ export default function Skills(props) {
 const Theme1 = () => {
   return (
     <>
-      <Col xs={12} sm={12} md={12} lg={12} className="skill-column">
-          <Row className="skill-row">
-            <Fade bottom>
-              <Image className='skill-image' alt='ruby' src={Ruby}/>
-              <Image className='skill-image' alt='rails' src={Rails}/>
-              <Image className='skill-image' alt='NestJs' src={Nest}/>
-              <Image className='skill-image' alt='html' src={Html}/>
-              <Image className='skill-image' alt='css' src={Css}/>
-              <Image className='skill-image' alt='Typescript' src={Typescript}/>
-            </Fade>
-          </Row>
-
-          <Row className="skill-row">
-            <Fade bottom>
-              <Image className='skill-image' alt='rspec' src={Rspec}/>
-              <Image className='skill-image' alt='jest' src={Jest}/>
-              <Image className='skill-image' alt='postgres' src={Postgres}/>
-              <Image className='skill-image' alt='mongo' src={Mongo}/>
-              <Image className='skill-image' alt='git' src={Git}/>
-            </Fade>
-          </Row>
-
-          <Row className="skill-row">
-            <Fade bottom>
-              <Image className='skill-image' alt='github' src={Github}/>
-              <Image className='skill-image' alt='bitbucket' src={Bitbucket}/>
-              <Image className='skill-image' alt='awss3t' src={Awss3}/>
-              <Image className='skill-image' alt='scrum' src={Scrum}/>
-              <Image className='skill-image' alt='jira' src={Jira}/>
-              <Image className='skill-image' alt='trello' src={Trello}/>
-            </Fade>
-          </Row>
+      <Col className="skill-column">
+        <Row className="skill-row" xs={3} sm={4} lg={6}>
+          <Col>
+            <Image className="skill-image" alt="ruby" src={Ruby} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="rails" src={Rails} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="NestJs" src={Nest} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="html" src={Html} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="css" src={Css} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="Typescript" src={Typescript} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="rspec" src={Rspec} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="jest" src={Jest} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="postgres" src={Postgres} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="mongo" src={Mongo} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="git" src={Git} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="github" src={Github} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="bitbucket" src={Bitbucket} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="awss3t" src={Awss3} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="scrum" src={Scrum} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="jira" src={Jira} />
+          </Col>
+          <Col>
+            <Image className="skill-image" alt="trello" src={Trello} />
+          </Col>
+        </Row>
       </Col>
     </>
-  )
-}
+  );
+};
