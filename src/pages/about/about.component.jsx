ef7638ef1ba0920cad25e19885eda36c643dc378
CamiMeneses <CamiMeneses@github.com>
2024-01-07 21:55:19 -0500

    modify about style

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Image from 'react-bootstrap/Image'
-import Profile from '../../assets/img/profile/profile.png'
-import Fade from 'react-reveal/Fade'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Fade from "react-reveal/Fade";
 
-import './about.style.scss'
+import Profile from "../../assets/img/profile/profile.png";
+import "./about.style.scss";
 
 const About = () => {
   return (
-    <div className='about-container'>
-      <div id='about' className='section'>
-      <div id='about-form-1' />
+    <div className="about-container">
+      <div id="about" className="section">
+        <div id="about-form-1" />
         <Container>
-          <Row className='align-items-center'>
-            <Col xs={12} sm={12} md={12} lg={12} xl={6} className="nospace">
+          <Row className="align-items-center">
+            <Col xs={12} sm={12} md={6} lg={6} xl={6} className="nospace">
               <Fade left>
-                <Row className='justify-content-center mb-2'>
-                  <Image className='profile justify-content-end nospace' alt='profile' src={Profile} roundedCircle fluid />
+                <Row className="justify-content-center">
+                  <Image
+                    className="profile justify-content-end nospace"
+                    alt="profile"
+                    src={Profile}
+                    roundedCircle
+                    fluid
+                  />
                 </Row>
               </Fade>
               <Fade left>
-                <Row className='justify-content-center mb-2 nospace'>
+                <Row className="justify-content-center nospace">
                   <h1>Camila Meneses</h1>
                   <h2>Mechatronics engineer and FullStack developer</h2>
                   <br />
@@ -30,24 +36,35 @@ const About = () => {
               </Fade>
             </Col>
             {/*Description*/}
-            <Col xs={12} sm={12} md={12} lg={12} xl={6}>
+            <Col xs={12} sm={12} md={6} lg={6} xl={6}>
               <Fade right>
-                <Row className=' align-items-start rounded profile-text'>
+                <Row className=" align-items-start rounded profile-text">
                   <p>
-                    I began my journey as a Web Developer in <b>2019</b>, since then I've been working mainly as a <b>Ruby on Rails</b> backend developer. 
+                    I began my journey as a Web Developer in <b>2019</b>, since
+                    then I've been working mainly as a <b>Ruby on Rails</b>{" "}
+                    backend developer.
                     <br />
                     <br />
-                    In my daily routine I use <b>Git</b>, <b>Postman</b>, I manage databases
-                    and code hosting platforms such as <b>GitHub</b> and <b>Bitbucket</b>
+                    In my daily routine I use <b>Git</b>, <b>Postman</b>, I
+                    manage databases and code hosting platforms such as{" "}
+                    <b>GitHub</b> and <b>Bitbucket</b>
                     <br />
                     <br />
-                    I keep learning about this world of development <br /> and I want to work hand in hand with best developers and <br />people of all cultures to grow in many ways.<br />
+                    I keep learning about this world of development and I want
+                    to work hand in hand with best developers and people of all
+                    cultures to grow in many ways.
+                    <br />
                     I am going to take any challenge!
                     <br />
                     <br />
-
                     Check out my website, learn more about me <br />
-                    and feel free to <b><a  href="#contact" target='contact me'>contact me</a> </b>. <br />
+                    and feel free to{" "}
+                    <b>
+                      <a href="#contact" target="contact me">
+                        contact me
+                      </a>{" "}
+                    </b>
+                    . <br />
                     <br />
                   </p>
                 </Row>
@@ -57,7 +74,7 @@ const About = () => {
         </Container>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
