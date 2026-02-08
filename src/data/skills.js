d5ae97aab8eb47a7eea98cb1fbda00643fd58fb3
Cami Meneses <kam_1095@live.com>
2026-02-07 22:05:10 -0500

    refactor skills and experience

---
+++ src/data/skills.js
@@ -0,0 +1,37 @@
+import Ruby from "../assets/icons/skills/ruby.png";
+import Rails from "../assets/icons/skills/rails.png";
+import Nest from "../assets/icons/skills/nest.png";
+import Html from "../assets/icons/skills/html.png";
+import Css from "../assets/icons/skills/css.png";
+import Typescript from "../assets/icons/skills/typescript.png";
+import Rspec from "../assets/icons/skills/rspec.png";
+import Jest from "../assets/icons/skills/jest.png";
+import Postgres from "../assets/icons/skills/postgres.png";
+import Mongo from "../assets/icons/skills/mongo.png";
+import Git from "../assets/icons/skills/git.webp";
+import Github from "../assets/icons/skills/github.png";
+import Bitbucket from "../assets/icons/skills/bitbucket.png";
+import Awss3 from "../assets/icons/skills/awss3.png";
+import Scrum from "../assets/icons/skills/scrum.png";
+import Jira from "../assets/icons/skills/jira.png";
+import Trello from "../assets/icons/skills/trello.png";
+
+export const skills = [
+  { id: "ruby", name: "Ruby", icon: Ruby },
+  { id: "rails", name: "Rails", icon: Rails },
+  { id: "nest", name: "NestJs", icon: Nest },
+  { id: "html", name: "HTML", icon: Html },
+  { id: "css", name: "CSS", icon: Css },
+  { id: "typescript", name: "TypeScript", icon: Typescript },
+  { id: "rspec", name: "RSpec", icon: Rspec },
+  { id: "jest", name: "Jest", icon: Jest },
+  { id: "postgres", name: "PostgreSQL", icon: Postgres },
+  { id: "mongo", name: "MongoDB", icon: Mongo },
+  { id: "git", name: "Git", icon: Git },
+  { id: "github", name: "GitHub", icon: Github },
+  { id: "bitbucket", name: "Bitbucket", icon: Bitbucket },
+  { id: "awss3", name: "AWS S3", icon: Awss3 },
+  { id: "scrum", name: "Scrum", icon: Scrum },
+  { id: "jira", name: "Jira", icon: Jira },
+  { id: "trello", name: "Trello", icon: Trello },
+];
--- src/pages/skills/skills.component.jsx
+++ src/pages/skills/skills.component.jsx
@@ -1,33 +1,11 @@
 import React from "react";
 import "./skills.style.scss";
-
-// Layout
 import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
-// Images
-import Ruby from "../../assets/icons/skills/ruby.png";
-import Rails from "../../assets/icons/skills/rails.png";
-import Nest from "../../assets/icons/skills/nest.png";
-import Html from "../../assets/icons/skills/html.png";
-import Css from "../../assets/icons/skills/css.png";
-import Typescript from "../../assets/icons/skills/typescript.png";
-import Rspec from "../../assets/icons/skills/rspec.png";
-import Jest from "../../assets/icons/skills/jest.png";
-
-import Postgres from "../../assets/icons/skills/postgres.png";
-import Mongo from "../../assets/icons/skills/mongo.png";
-
-import Git from "../../assets/icons/skills/git.webp";
-import Github from "../../assets/icons/skills/github.png";
-import Bitbucket from "../../assets/icons/skills/bitbucket.png";
-import Awss3 from "../../assets/icons/skills/awss3.png";
-
-import Scrum from "../../assets/icons/skills/scrum.png";
-import Jira from "../../assets/icons/skills/jira.png";
-import Trello from "../../assets/icons/skills/trello.png";
+import { skills } from "../../data/skills";
 
 const Skills = () => {
   return (
@@ -44,119 +22,23 @@ const Skills = () => {
   );
 };
 
-export default Skills;
-
 const SkillsGrid = () => {
   return (
     <Col className="skill-column">
       <Row className="skill-row" xs={3} sm={4} lg={6}>
-        <Col>
-          <Image className="skill-image" alt="ruby" src={Ruby} loading="lazy" />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="rails"
-            src={Rails}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="NestJs"
-            src={Nest}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image className="skill-image" alt="html" src={Html} loading="lazy" />
-        </Col>
-        <Col>
-          <Image className="skill-image" alt="css" src={Css} loading="lazy" />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="typescript"
-            src={Typescript}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="rspec"
-            src={Rspec}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image className="skill-image" alt="jest" src={Jest} loading="lazy" />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="postgres"
-            src={Postgres}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="mongo"
-            src={Mongo}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image className="skill-image" alt="git" src={Git} loading="lazy" />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="github"
-            src={Github}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="bitbucket"
-            src={Bitbucket}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="aws s3"
-            src={Awss3}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="scrum"
-            src={Scrum}
-            loading="lazy"
-          />
-        </Col>
-        <Col>
-          <Image className="skill-image" alt="jira" src={Jira} loading="lazy" />
-        </Col>
-        <Col>
-          <Image
-            className="skill-image"
-            alt="trello"
-            src={Trello}
-            loading="lazy"
-          />
-        </Col>
+        {skills.map((skill) => (
+          <Col key={skill.id}>
+            <Image
+              className="skill-image"
+              alt={skill.name}
+              src={skill.icon}
+              loading="lazy"
+            />
+          </Col>
+        ))}
       </Row>
     </Col>
   );
 };
+
+export default Skills;
